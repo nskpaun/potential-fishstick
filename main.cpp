642ae487863ff9c72b9d12218bd9851a9fe19c35
Nathan Spaun <nkspaun@gmail.com>
2023-09-23 21:42:23 -0700

    Add some actual vulkan code

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <iostream>
 
 #include "model/GameModel.h"
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
 
 int main() {
     std::cout << "Hello PF GAME!" << std::endl;
@@ -21,6 +25,11 @@ int main() {
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    std::cout << extensionCount << " extensions supported\n";
+
     if (!window)
     {
         glfwTerminate();
